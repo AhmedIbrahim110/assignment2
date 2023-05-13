b66fb80e04bfc5ccd090536dcf72a59f692e24b1
DerRiese <DerRiese765@gmail.com>
2023-05-13 21:35:21 +0300

    -

--- inc/form.php
+++ inc/form.php
@@ -10,31 +10,43 @@ if(isset($_POST['firstName'])){
     $email = $_POST['email'];
     }
 
+$errors =[
+    'firstNameError' => '',
+    'lastNameError' => '',
+    'emailError' => '',
+];
     
 if(isset($_POST['submit'])){
 
 
-    $firstName = mysqli_real_escape_string($conn, $_POST['firstName']);
-    $lastName =  mysqli_real_escape_string($conn, $_POST['lastName']);
-    $email =     mysqli_real_escape_string($conn, $_POST['email']);
 
-    $sql = "INSERT INTO users(firstName, lastName, email)
-    VALUES ('$firstName', '$lastName', '$email')";
 
+
+    //تحقق الاسم الاول
     if(empty($firstName)){
-        echo 'Enter first name';
-    }elseif(empty($lastName)){
-        echo 'Enter Last name';
-    }elseif(empty($email)){
-        echo 'Enter Email';
-    }elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        echo 'enter a vaild email';
+        $errors['firstNameError'] = 'يرجى ادخال الأسم الاول';
     }
-    else{
-        if(mysqli_query($conn, $sql)){
-    header('Location: index.php');
-}else{
-    echo 'Error: ' . mysqli_error($conn);
-}
-}
-}
+    //تحقق الاسم الاخير
+    if(empty($lastName)){
+        $errors['lastNameError'] = 'يرجى ادخال الأسم الاخير';
+    }
+    //تحقق الايميل
+    if(empty($email)){
+        $errors['emailError'] = 'يرجى ادخال الإيميل';
+    }elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $errors['emailError'] = 'يرجى ادخال إيميل صحيح ';
+    }
+    //تحقق لا يوجد اخطاء
+    if(!array_filter($errors)){
+        $firstName = mysqli_real_escape_string($conn, $_POST['firstName']);
+        $lastName =  mysqli_real_escape_string($conn, $_POST['lastName']);
+        $email =     mysqli_real_escape_string($conn, $_POST['email']);
+        $sql = "INSERT INTO users(firstName, lastName, email)
+        VALUES ('$firstName', '$lastName', '$email')";
+                if(mysqli_query($conn, $sql)){
+                    header('Location: index.php');
+                }else{
+                    echo 'Error: ' . mysqli_error($conn);
+                }
+                }
+    }
--- index.php
+++ index.php
@@ -30,7 +30,7 @@ mysqli_close($conn);
 
 <body>
     <div class="container">
-
+<!---
     <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
     <div class="col-md-5 p-lg-5 mx-auto my-5">
       <h1 class="display-4 fw-normal">اربح مع احمد</h1>
@@ -43,38 +43,33 @@ mysqli_close($conn);
   <li class="list-group-item">تابع عالفيسبوك</li>
   <li class="list-group-item">السحب سيكون عشوائي</li>
 </ul>
-
-<form class="mt-5">
+-->
+<form class="mt-5" action="index.php" method="POST">
 <h3>الرجاء ادخال معلوماتك</h3>
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">الإسم الاول</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text error"></div>
+    <label for="firstName" class="form-label">الإسم الاول</label>
+    <input type="text" name="firstName" class="form-control" id="firstName">
+    <div id="emailHelp" class="form-text error"><?php echo $errors['firstNameError'] ?></div>
   </div>
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">الإسم الأخير</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text error"></div>
+    <label for="lastName" class="form-label">الإسم الأخير</label>
+    <input type="text" name="lastName" class="form-control" id="lastName">
+    <div id="emailHelp" class="form-text error"><?php echo $errors['lastNameError'] ?></div>
   </div>
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">البريد الإلكتروني</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text error"></div>
+    <label for="email" class="form-label">البريد الإلكتروني</label>
+    <input type="text" name="email" class="form-control" id="email">
+    <div id="emailHelp" class="form-text error"><?php echo $errors['emailError'] ?></div>
+    <button type ="submit" name="submit" class="btn btn-primary">ارسال المعلومات</button>
   </div>
 </form>
 
 </div>
-
-<form action="index.php" method="POST">
-    <input type="text" name="firstName" id="firstName" placeholder="First Name">
-    <input type="text" name="lastName" id="lastName" placeholder="Last Name">
-    <input type="text" name="email" id="email" placeholder = "Email">
-    <input type="submit" name="submit" value="send">
-</form>
+<!----
 <?php foreach($users as $user): ?>
     <h1><?php echo htmlspecialchars($user['firstName']) . ' ' . htmlspecialchars($user['lastName']) . '<br>' . htmlspecialchars($user['email'])?></h1>
 <?php endforeach; ?>
-    
+--->
 <script src="./js/bootstrap.bundle.min.js"></script>
 <script src="./js/script.js"></script>
 </body>
