33e2b6d0a8ff852bf811cb8d4447b6b4d956235d
DerRiese <DerRiese765@gmail.com>
2023-05-13 23:23:03 +0300

    -

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ include'./inc/select.php'
     <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
     <div class="col-md-5 p-lg-5 mx-auto my-5">
       <h1 class="display-4 fw-normal">اربح مع احمد</h1>
+      <h3 id="countdown"></h3>
       <p class="lead fw-normal">باقي على فتح التسجيل</p>
       <a class="btn btn-outline-secondary" href="#">Coming soon</a>
     </div>
